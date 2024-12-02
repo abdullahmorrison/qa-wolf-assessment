f5095c6bd0af55fbc5063db93de6959e3c2ddfcf
Abdullah Morrison <abdullahmorrison@gmail.com>
2024-12-02 18:36:15 -0500

    completed assignment

--- index.js
+++ index.js
@@ -9,6 +9,33 @@ async function sortHackerNewsArticles() {
 
   // go to Hacker News
   await page.goto("https://news.ycombinator.com/newest");
+
+  await page.waitForSelector('.athing');
+
+  const times = []
+
+  while(times.length<100){
+    const articleTimes = await page.evaluate(async () => {
+      const articles = document.querySelectorAll('.athing');
+
+      return Array.from(articles).map(article => {
+        const timeElement = article.nextElementSibling.querySelector('.age');
+        return timeElement ? timeElement.getAttribute('title').split(" ")[0] : null;
+      })
+    })
+
+    times.push(...articleTimes)
+    await page.locator('.morelink').click()
+  }
+
+  const isSortedDescending = times.slice(0,100).every((time, index) =>
+    index === 0 || new Date(time) <= new Date(times[index - 1])
+  )
+
+  console.log('Articles sorted correctly:', isSortedDescending);
+
+  await browser.close();
+  return isSortedDescending;
 }
 
 (async () => {
